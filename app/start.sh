90933ed22021cb268e5d1362abca5f44aa6f5a07
Ryan Fobel <ryan@fobel.net>
2020-05-20 01:09:58 -0400

    Make input/output plugins configurable via env variables

--- app/start.sh
+++ app/start.sh
@@ -9,8 +9,15 @@ service haproxy start >/dev/null 2>&1 || true
 # Make sure mpjg-streamer can find the plugins
 export LD_LIBRARY_PATH="/usr/src/app/mjpg-streamer/mjpg-streamer-experimental"
 
-# start picam stream
-cd /usr/src/app/mjpg-streamer/mjpg-streamer-experimental/ && ./mjpg_streamer -i "input_raspicam.so" -o "output_http.so -w `pwd`/www" -b
+# picam
+#input=${INPUT_PLUGIN:-"input_raspicam.so"}
+
+# usb webcam
+input=${INPUT_PLUGIN:-"input_uvc.so -n -f 10 -r 1280x720"}
+
+output=${OUTPUT_PLUGIN:-"output_http.so -p 8085 -w /usr/local/share/mjpg-streamer/www"}
+
+mjpg_streamer -i "$input" -o "$output" -b
 
 # start Octoprint
 octoprint serve --iknowwhatimdoing --port=5000 --basedir /data
